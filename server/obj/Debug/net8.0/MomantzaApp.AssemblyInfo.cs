1025578ef9457aa9086ded9317020d2517474018
Roshan <roshantamilselvam@gmail.com>
2025-12-22 18:31:39 +0530

    fix

--- server/obj/Debug/net8.0/MomantzaApp.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/MomantzaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5faad16c4b104222b6bc20279b46c742c824b042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65de9ab8f5515cd109ae0046bb69a7e26257f8d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
