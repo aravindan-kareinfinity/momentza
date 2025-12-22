5faad16c4b104222b6bc20279b46c742c824b042
aravindan-kareinfinity <aravindan.m@kareinfinity.com>
2025-12-22 10:29:07 +0530

    1

--- server/obj/Release/net8.0/MomantzaApp.AssemblyInfo.cs
+++ server/obj/Release/net8.0/MomantzaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120b5813abb3816e4877717ab43c693fbd87fa1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275ccd40dc031bd00350349cc682712e25d22317")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomantzaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
